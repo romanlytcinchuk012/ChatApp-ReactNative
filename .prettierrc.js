64c76c4a93ce2a00204f1a53159c6644fe4cb32e
Bestguy900 <VitaliiBusiness990@gmail.com>
2021-06-17 10:21:55 +0400

    Updating the version

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'none',
+  arrowParens: 'avoid',
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  jsxBracketSameLine: false,
+  bracketSpacing: true,
+  tabWidth: 2
+};
